50b0214595503c57b7f06172a11e4089910ae589
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-04 19:30:04 -0700

    Question 4

--- bug1.py
+++ bug1.py
@@ -1,33 +1,34 @@
 class Base:
-    def __init__(self, x, y, size):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class Shape(Base):
+    def shape(self):
+        return f"This is a {self.__class__.__name__.lower()} ({self.x}, {self.y})"
+
+class Square(Shape):
+    def __init__(self, x, y, size):
+        super().__init__(x, y)
         self.size = size
 
+    def draw(self):
+        return f"{super().shape()}\n{'#' * self.size}"
 
-class Circle(Base):
+class Circle(Shape):
     def __init__(self, x, y, size):
-        super().__init__(x, y, size)
+        super().__init__(x, y)
+        self.size = size
 
     def draw(self):
-        return f"""
-        ({self.x}, {self.y}) {self.size}
-        ,-~~~-,
-        ,' ', ,, ,, ,, ,, ,, ,, ,,
-        ,,' '-,___, '
-        """
-
-
-class Square(Base):
-    def shape(self):
-        return "This is a square"
-
+        return f"{super().shape()}\n,-~~~-,\n,' ', ,, ,, ,, ,, ,, ,, ,,\n,,' '-,___, 
+'"
 
 def main():
     s = Square(1, 2, 3)
     print(s.shape())
     print(s.draw())
 
-
 if __name__ == "__main__":
     main()
+
