07f879ba68e67d7e21cfd5e3e731d5e1a8085168
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-05 08:19:40 -0700

    Question 7b

--- log.py
+++ log.py
@@ -1,19 +1,15 @@
 import time
 
 def timestamp(func):
+
     def wrapper(*args, **kwargs):
+
         current_time = time.ctime()
+	
+	print(current_time)
+
         result = func(*args, **kwargs)
-        print(f"Timestamp: {current_time}")
-        print(f"Decorated Content: {result}")
+        
+        
         return result
     return wrapper
-
-
-from log import timestamp
-
-
-def example_function():
-    return "This is an example function."
-
-result = example_function()
