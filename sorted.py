700aec2d3c431cdb1c5db821b6aac7318c3a1b36
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-05 09:25:06 -0700

    Question 3

--- sorted.py
+++ sorted.py
@@ -1,9 +1,9 @@
-def sort_dictionary(d):
-    if not isinstance(d, dict):
-        raise ValueError("Not valid dictionary.")
+def sort_dictionary(d): 
+	if not isinstance (d, dict):
+		raise ValueError ("not valid dictionary.")
 
-    sorted_d = sorted(d.items, key=lambda x: x[0], reverse=True)
-    result_list = [(name, data[0]) for name, data in sorted_d]
+	sorted_d = sorted(d. items, key=lambda x: x[0],reverse=True)
+	result_list = [(name, data[0]) for name, data in sorted _d]
 
-    return result_list
+	return result_list 
 
