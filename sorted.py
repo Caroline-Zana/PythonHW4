fb90a252fe1af16c2dc8c5a04ec7a17816b08b0a
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-06 09:34:08 -0700

    Question 3

--- sorted.py
+++ sorted.py
@@ -3,3 +3,9 @@ def sort_dictionary(d):
     sorted_list = [(k, v[0]) for k, v in sorted_dict]
     return sorted_list
 
+my_dict = {"Tom":(5464512, 24), "Sara":(5446987, 32), "Mary":(1557896, 20)}
+sorted_list = sort_dictionary(my_dict)
+
+output = [tup for tup in reversed(sorted_list)]
+print(output)
+
