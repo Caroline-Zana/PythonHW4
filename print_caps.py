6e1edbde01c2618062bf6821c8b0cb17c41f5df4
Caroline Zana <caroline.zana@sjsu.edu>
2023-10-06 09:59:58 -0700

    Question 7

--- print_caps.py
+++ print_caps.py
@@ -1,6 +1,6 @@
 def allcaps(func):
-	def wrapper():
-		result = func()
-		return result.upper()
-	return wrapper
+    def wrapper():
+        result = func()
+        print(result.upper())
 
+    return wrapper
